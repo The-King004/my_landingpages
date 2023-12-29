b6f13bf450e1f6490be3e68b09ab652af81b91fd
The-king004 <ahmedadama231@gmail.com>
2023-12-29 17:52:01 +0100

    version 0.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,32 @@ const navlinks = document.querySelector(".navlinks")
 
 menuheader.addEventListener("click", ()=>{
     navlinks.classList.toggle('mobile-menu')
-})
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+      "@2.00": {
+        slidesPerView: 5,
+        spaceBetween: 60,
+      },
+    },
+  });
